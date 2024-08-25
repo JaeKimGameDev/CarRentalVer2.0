6efb73c65bb1cf2fc1a9695b3ea4cb06f0c01573
Jae <55465890+kimj80@users.noreply.github.com>
2024-08-25 01:33:08 -0600

    Ability to insert into table
    Ability to insert into table

--- Main.py
+++ Main.py
@@ -78,13 +78,13 @@ def insertBranchData(branch_id, branch_name, area_code, contact_number, street, 
 
     return
 
-def insertEmployeeData(employee_id, branch_id, operator_id, first_name, last_name, street, postal_code, dob, area_code, contact_number, password):
+def insertEmployeeData(employee_id, branch_id, operator_id, first_name, last_name, street, postal_code, dob, area_code, contact_number, password, active):
     try:
         mySql_insert_query = ("INSERT INTO Employee "
-                              "(EmployeeID, BranchID, OperatorID, FirstName, LastName, Street, PostalCode, DOB, AreaCode, ContactNumber, Password) " 
-                              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ")
+                              "(EmployeeID, BranchID, OperatorID, FirstName, LastName, Street, PostalCode, DOB, AreaCode, ContactNumber, Password, Active) " 
+                              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ")
 
-        record = (employee_id, branch_id, operator_id, first_name, last_name, street, postal_code, dob, area_code, contact_number, password)
+        record = (employee_id, branch_id, operator_id, first_name, last_name, street, postal_code, dob, area_code, contact_number, password, active)
         cursor.execute(mySql_insert_query, record)
         mydb.commit()
         print("Record inserted successfully into table")
@@ -141,7 +141,7 @@ def createDataForCarType():
                 cabin_size = t
                 for y in range(3):
                     engine = y
-                    for u in range(3):
+                    for u in range(4):
                         drivetrain = u
                         for o in range(3):
                             trim = o
@@ -150,9 +150,137 @@ def createDataForCarType():
                             car_type_id += 1
     return
 
-def main():
-    createDataForCarType()
+def createDataForBranch():
+    branch_id=0
+    first_half_branch_name="USA branch name test "
+    second_half_branch_name=0
+    area_code = 100
+    contact_number = 1000000
+    street="Somewhere in USA"
+    # 5 digit zip code
+    zip_code = "zfive"
+    #700 cities
+    city = 0
+    # 50 states
+    state=0
+    country=0
+    for i in range(5000):
+        branch_name = first_half_branch_name + str(second_half_branch_name)
+        insertBranchData(branch_id, branch_name, area_code, contact_number, street, zip_code, city, state, country)
+        branch_id += 1
+        contact_number += 1
+        city+=1
+        if (city > 700):
+            city=0
+        state+=1
+        if (state > 50):
+            state=0
+        second_half_branch_name+=1
 
+    # start Canada at 10,000 for USA branches to keep adding onto the tail end, adding consistency
+    branch_id = 10000
+    first_half_branch_name="CANADA branch name test "
+    second_half_branch_name=0
+    area_code = 200
+    contact_number = 2000000
+    street="Somewhere in CANADA"
+    # Postal Code
+    postal_code = "T1T1T1"
+    # 100 cities
+    city = 1000
+    # 10 Provinces
+    province=100
+    country=1
+    for k in range(3000):
+        branch_name = first_half_branch_name + str(second_half_branch_name)
+        insertBranchData(branch_id, branch_name, area_code, contact_number, street, postal_code, city, province, country)
+        branch_id += 1
+        contact_number += 1
+        city+=1
+        if (city > 1100):
+            city=1000
+        province+=1
+        if (province > 109):
+            province=100
+        second_half_branch_name+=1
+
+    return
+
+def createDataForEmployee():
+    # insertEmployeeData(employee_id, branch_id, operator_id, first_name, last_name, street, postal_code, dob, area_code,
+    #                    contact_number, password, active):
+    employee_id=0
+    branch_id=0
+    operator_id = 20000000
+    street="Some street for customer"
+    postal_code="T1T1T1"
+    #YYYY-MM-DD
+    # range days up to 28, months to 12, years from 1960
+    day=0
+    month=1
+    year=1959
+    area_code=200
+    contact_number=5000000
+    for i in range(1000):
+        operator_id+=1
+        first_name="first_name " + str(i)
+        last_name="last_name " + str(i)
+        password="password " + str(i)
+        day+=1
+        if (day > 28):
+            month+=1
+            day=1
+            if (month > 12):
+                month=1
+                year+=1
+        dob = str(year) + "-" + str(month) + "-" + str(day)
+        contact_number+=1
+
+        insertEmployeeData(employee_id, branch_id, operator_id, first_name, last_name, street, postal_code, dob, area_code, contact_number, password, 1)
+        employee_id+=1
+        branch_id+=1
+        if (branch_id > 4999):
+            branch_id=0
+
+    return
+
+def createDataForCustomer():
+    customer_id=0
+    operator_id=10000000
+    street="Some street for customer"
+    postal_code="T1T1T1"
+    #YYYY-MM-DD
+    # range days up to 28, months to 12, years from 1960
+    day=0
+    month=1
+    year=1959
+    area_code=200
+    contact_number=4000000
+    for i in range(4000):
+        operator_id+=1
+        first_name="first_name " + str(i)
+        last_name="last_name " + str(i)
+        password="password " + str(i)
+        day+=1
+        if (day > 28):
+            month+=1
+            day=1
+            if (month > 12):
+                month=1
+                year+=1
+        dob = str(year) + "-" + str(month) + "-" + str(day)
+        contact_number+=1
+
+        insertCustomerData(customer_id, operator_id, first_name, last_name, street, postal_code, dob, area_code, contact_number, password)
+        customer_id+=1
+
+    return
+
+def main():
+    #createDataForCarType()
+    #createDataForBranch()
+    #createDataForEmployee()
+    #createDataForCustomer()
     if mydb.is_connected():
         cursor.close()
         mydb.close()
